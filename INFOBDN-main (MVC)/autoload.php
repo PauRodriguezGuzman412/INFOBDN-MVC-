179f50c15f8faa6c897ad8c62fa97c9ca77f38a0
PauRodriguezGuzman412 <113432167+PauRodriguezGuzman412@users.noreply.github.com>
2023-02-13 20:29:32 +0100

    Delete INFOBDN-main (MVC) directory

--- INFOBDN-main (MVC)/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function autocargar($nombreClase){
-    include "controllers/$nombreClase.php";
-}
-spl_autoload_register("autocargar");
-
-?>
-
--- INFOBDN-main (MVC)/views/general/SignOut.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_destroy();
-?>
-
-<META HTTP-EQUIV="REFRESH" CONTENT="0;URL=index.php?controller=user&action=mostrarHome">
